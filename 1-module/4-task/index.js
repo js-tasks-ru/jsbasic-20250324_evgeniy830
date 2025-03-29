46b7c4424f3f97ea16b5b2e065a2dc7e476e2654
Sneakson <sneaksonx@gmail.com>
2025-03-29 21:43:17 +0300

    Added solutions

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,18 @@
 function checkSpam(str) {
-  // ваш код...
+  // Более гибкое решение с возможностью расширения списка строк
+  const forbiddenWords = [
+    '1xbet',
+    'xxx'
+  ];
+  let lowerCaseStr = str.toLowerCase();
+  for (let word of forbiddenWords) {
+    if (lowerCaseStr.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+
+  // Более простое решение для 2 строк
+  // let lowerCaseStr = str.toLowerCase();
+  // return lowerCaseStr.includes('1xbet') || str.includes('xxx');
 }
